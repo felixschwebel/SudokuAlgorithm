cebbafc9b763656735689b396b458358478ab3df
felixschwebel <schwebel.felix@gmail.com>
2022-12-04 18:37:16 +0100

    populate the Sudoku field and set up the structure

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
 import tkinter as tk
+from sudoku_solver import SudokuSolver
 
 FONT = ('Times New Roman', 24, "normal")
 
+# function to get the inputs
+def get_inputs():
+    # Check all the Entry fields and put the values in a dictionary
+    entry_data = {}
+    return entry_data
+
+# Solve Sudoku
+def solve():
+    # uses the SudokuSolver Class
+    solver = SudokuSolver(get_inputs())
+    # displays the found values
+
+# Set up the Window for Tkinter
 window = tk.Tk()
 window.title('Sudoku Solving')
 window.config(bg="white")
 window.minsize()
 
+# Canvas class from tkinter to get the Sudo-grid as background image
 canvas = tk.Canvas(width=504, height=504)
 sudoku_grid = tk.PhotoImage(file="sudoku_grid.png")
 canvas.create_image(252, 252, image=sudoku_grid)
+canvas.config(borderwidth=0, highlightthickness=0)
 canvas.grid(column=0, columnspan=81, row=0, rowspan=81, padx=20, pady=20)
 
-new_input = tk.Entry(width=2, font=FONT)
-canvas.create_window(31, 30, window=new_input)
+# Create a list of Entries to populate the Sudoku-field
+input_list = []
+list_index = 0
+x_start = 31
+y_start = 31
 
-new_input2 = tk.Entry(width=2, font=FONT)
-canvas.create_window(31, 86, window=new_input2)
+for i in range(9):
+    for j in range(9):
+        input_list.append(tk.Entry(width=1, font=FONT, fg='black', bg='white',
+                                   borderwidth=0, highlightthickness=0))
+        canvas.create_window(x_start, y_start, window=input_list[list_index])
+        list_index += 1
+        x_start += 55
+    x_start = 31
+    y_start += 55
 
-new_input3 = tk.Entry(width=2, font=FONT)
-canvas.create_window(86, 31, window=new_input3)
-
-# input_list = []
-# for i in range(81):
-#     new_input =
-
-button = tk.Button(text='Solve', bg='black')
+# button to start the calculation
+button = tk.Button(text='Solve', bg='black', command=solve)
 button.config(highlightthickness=0, highlightbackground='white', fg='red')
 button.grid(column=0, row=82, padx=20, pady=20)
 
---
+++ sudoku_solver.py
@@ -0,0 +1,3 @@
+class SudokuSolver:
+    def __init__(self, entry_data):
+        data = entry_data
